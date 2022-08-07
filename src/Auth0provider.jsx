f4d583c11e3b24bbca1e35e2a640c7fef0867e60
Yonesxt <Yones.rpy@gmail.com>
2022-08-07 15:16:21 -0300

    front

--- src/Auth0provider.jsx
+++ src/Auth0provider.jsx
@@ -7,10 +7,10 @@ export default function ({children}) {
 
     return (
         <Auth0Provider
-        domain='https://dev-81nqhdy2.us.auth0.com'
+        domain='http://dev-81nqhdy2.us.auth0.com'
         clientId='A6q8eeIevBO37mbaercJOAcuJU1LRRns'
         redirectUri={window.location.origin}
-        audience= "https://dev-81nqhdy2.us.auth0.com/api/v2/"
+        audience= "http://dev-81nqhdy2.us.auth0.com/api/v2/"
         //Se necesita el audience
         >
             {children}
